1cadf2f5f14beabb8a5c230d2faa8f872aa59eee
Katon Adams Fitrianto <33994686+katon26@users.noreply.github.com>
2021-10-15 22:10:54 +0700

    Improving first contentful paint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "bootstrap"; // import JS
 import "bootstrap/dist/css/bootstrap.min.css"; // import CSS
 import './App.css';
-import Feature from "./components/Feature";
-import Intro from "./components/Intro";
+import React, { Suspense, lazy } from "react";
 import Navbar from './components/Navbar';
-import Content from "./components/Content";
-import SlideContent from "./components/SlideContent";
-import JoinUs from "./components/JoinUs";
 import Footer from "./components/Footer";
+
+// Improve first contentful paint in React
+const Feature = lazy(() => import ('./components/Feature'));
+const Intro = lazy(() => import ('./components/Intro'));
+const Content = lazy(() => import ('./components/Content'));
+const SlideContent = lazy(() => import ('./components/SlideContent'));
+const JoinUs = lazy(() => import ('./components/JoinUs'));
 
 function App() {
   return (
